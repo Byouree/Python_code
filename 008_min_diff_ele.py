a29bc09c093095ed6f64a32a6e22de8fc7108936
ByuhLee <79687199+ByuhLee@users.noreply.github.com>
2021-12-03 09:49:13 -0800

    Create 008_min_diff_ele.py

---
+++ 008_min_diff_ele.py
@@ -0,0 +1,14 @@
+def min_diff_ele(arr):
+    arr=sorted(arr)
+    size = len(arr)
+    min_diff = 9999*999
+
+    for i in range(size-1):
+        if arr[i+1]-arr[i] < min_diff:
+            min_diff = arr[i+1] - arr[i]
+
+    return min_diff
+
+
+arr=[5,32,45,3,12,18,25]
+print(min_diff_ele(arr))
