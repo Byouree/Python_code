2f0f6fc1a3690ca09b344bf946c823349021f74f
ByuhLee <79687199+ByuhLee@users.noreply.github.com>
2021-12-03 23:07:21 -0800

    Create 012_hotel_book.py

---
+++ 012_hotel_book.py
@@ -0,0 +1,36 @@
+def hotel_book(arr, dep, k):
+    event = []
+    for i in range(0, len(arr)):
+        t_arrive = ()
+        t_arrive = (arr[i], "RED")
+        event.append(t_arrive)
+    print(t_arrive)
+    print(event)
+
+    for i in range(0, len(dep)):
+        t_departure = ()
+        t_departure = (dep[i], "BLUE")
+        event.append(t_departure)
+
+
+    event = sorted(event)
+    print(event)
+
+    guest = 0
+
+    for t in event:
+        if t[1] == "RED":
+            guest += 1
+        else:
+            guest -= 1
+        if guest > k:
+            return 0
+
+    return 1
+
+arr = [1,3,5]
+dep = [2,6,8]
+k = 2
+print(hotel_book(arr, dep, k))
+
+
