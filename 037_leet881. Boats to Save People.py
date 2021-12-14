6a55666f989cac718e1c336e2728e5720b35fb77
ByuhLee <79687199+ByuhLee@users.noreply.github.com>
2021-12-14 11:39:42 -0800

    Create 037_leet881. Boats to Save People.py

---
+++ 037_leet881. Boats to Save People.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        n=len(people)
+        people = sorted(people)
+        left = 0
+        right = n-1        
+        boat = 0
+        while left <= right:
+            if left == right:
+                boat+=1
+                break
+            if people[left]+people[right] <= limit:
+                boat += 1
+                left += 1
+                right -= 1
+            else:
+                boat += 1
+                right -= 1
+        
+        return boat
+                
+                
