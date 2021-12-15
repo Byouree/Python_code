971f52af43ec4a55342484b18077c45b05eea479
ByuhLee <79687199+ByuhLee@users.noreply.github.com>
2021-12-14 16:26:31 -0800

    Create 038_leet941_Valid Mountain Array.py

---
+++ 038_leet941_Valid Mountain Array.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        n = len(arr)
+        if n < 3:
+            return 0
+        
+        index = 0
+        
+        maxnum = arr[0]
+        for i in range(1,n):
+            if arr[i] >maxnum:
+                maxnum=arr[i]
+                index = i
+        print(index)
+        
+        left = 0
+        right = n-1
+        
+        if index == left or index == right:
+            return 0
+        
+        while left < index:
+            if arr[left] >= arr[left+1]:
+                return 0
+            left +=1
+        
+        while index < right:
+            if arr[index] <= arr[index+1]:
+                return 0
+            index +=1
+            
+        return 1
+    
+            
+        
