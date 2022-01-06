f29f6d033906615c9cdbd0399526036a66a72874
ByuhLee <79687199+ByuhLee@users.noreply.github.com>
2022-01-06 10:11:31 -0800

    Update 008_min_diff_ele.py

--- 008_min_diff_ele.py
+++ 008_min_diff_ele.py
@@ -12,3 +12,16 @@ def min_diff_ele(arr):
 
 arr=[5,32,45,3,12,18,25]
 print(min_diff_ele(arr))
+
+#######################################################
+def min_diff(arr):
+    arr = sorted(arr)
+    min_diff = arr[1]-arr[0]
+    for i in range(1, len(arr)-1):
+        if arr[i+1]-arr[i] < min_diff:
+            min_diff = arr[i+1]-arr[i]
+    return min_diff
+
+
+arr=[9,32,45,4,12,18,25]
+print(min_diff(arr))
