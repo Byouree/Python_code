4c39f263d275473bf22b1a4e1372d84f6b8864f5
ByuhLee <79687199+ByuhLee@users.noreply.github.com>
2022-01-06 10:15:04 -0800

    Update 009_max_diff_ele.py

--- 009_max_diff_ele.py
+++ 009_max_diff_ele.py
@@ -12,3 +12,17 @@ def max_diff_ele(arr):
 
 arr=[5,32,45,4,12,18,25]
 print(max_diff_ele(arr))
+
+
+######################################
+def max_diff(arr):
+    arr = sorted(arr)
+    max_diff = arr[1]-arr[0]
+    for i in range(1, len(arr)-1):
+        if arr[i+1]-arr[i] > max_diff:
+            max_diff = arr[i+1]-arr[i]
+    return max_diff
+
+
+arr=[5,40,45,4,12,18,25]
+print(max_diff(arr))
